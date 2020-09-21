92ef99fe709fadb26c46d07ccf53d9caecfe06b0
Pavel Petrov <rps.lob@gmail.com>
2020-09-21 18:52:31 +0300

    feature: Self update added.

--- src/cadrone_aliases/cadrone_aliases.sh
+++ src/cadrone_aliases/cadrone_aliases.sh
@@ -5,20 +5,57 @@ caversion () {
 }
 
 caupdate () {
-	# local OLD_VERSION=$CAVERSION
-	# wget --no-cache -qO ~/.cadrone_aliases https://gist.github.com/chaos-drone/5b7d8cbe01df6ab64dfce90f32ad3b70/raw/.cadrone_aliases
-	# source ~/.cadrone_aliases
-	# printf "\n\nAliases updated to version $CAVERSION. Previous version was $OLD_VERSION\n\n"
-
-    # 0.19.0 https://aliases.pavelpetrov.space/cadrone_aliases_0.19.0.tar.gz
-
     local LATEST_VERSION_DATA="$(wget --no-cache -qO- https://aliases.pavelpetrov.space/latest)"
+
+    if [ 0 -lt "$?" ]; then
+        echo "Error: Version check failed."
+        return 1
+    fi
+
     local LATEST_VERSION=$(echo $LATEST_VERSION_DATA | cut -d ' ' -f1)
 
     if [[ $LATEST_VERSION = $CAVERSION ]]; then
         echo "You already have the latest version $LATEST_VERSION."
-        return
+        return 2
     fi
 
-    echo "You don't have the latest version."
+    local DOWNLOAD_URL=$(echo $LATEST_VERSION_DATA | cut -d ' ' -f2)
+    local DOWNLOAD_PATH="$TMPDIR/$(basename -- $DOWNLOAD_URL)"
+
+    wget --no-cache -q -O $DOWNLOAD_PATH $DOWNLOAD_URL
+
+    if [ 0 -lt "$?" ]; then
+        echo "Error: Failed to download latest version."
+        return 1
+    fi
+
+    if [ -f "/opt/cadrone_aliases/source_cadrone_aliases.sh" ]; then
+        local INSTALLATION_PATH="/opt/cadrone_aliases"
+    elif [ -f "/c/cadrone_aliases/source_cadrone_aliases.sh" ]; then
+        local INSTALLATION_PATH="/c/cadrone_aliases"
+    elif [ -f "$HOME/cadrone_aliases/source_cadrone_aliases.sh" ]; then
+        #depricated
+        local INSTALLATION_PATH="$HOME/cadrone_aliases"
+    else
+        echo "Error: Cannot locate isntallation path."
+        return 1
+    fi
+
+    tar -x -f $DOWNLOAD_PATH -C $INSTALLATION_PATH
+
+    if [ 0 -lt "$?" ]; then
+        echo "Error: Failed to extract latest version from the downloaded file."
+        return 1
+    fi
+
+    rm -f $DOWNLOAD_PATH
+
+    source "$INSTALLATION_PATH/source_cadrone_aliases.sh"
+
+    if [ 0 -lt "$?" ]; then
+        echo "Error: Failed to load latest version in current shell."
+        return 1
+    fi
+
+    echo "Congratulations! You now have latest version $LATEST_VERSION of Cadrone Aliases installed."
 }
